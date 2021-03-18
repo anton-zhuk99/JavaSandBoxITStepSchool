86e4aae4f55e3ee0313424c3e05a7e2336e688ab
Anton Zhuk <54962677+anton-zhuk99@users.noreply.github.com>
2021-03-18 20:02:16 +0200

    Add I/O examples.

--- src/com/company/examples/module2/ex020_file/Main.java
+++ src/com/company/examples/module2/ex020_file/Main.java
@@ -1,14 +1,13 @@
 package com.company.examples.module2.ex020_file;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -26,7 +25,7 @@ public class Main {
         fileScanner.close();
     }
 
-    static void fileReadContent() throws IOException {
+    static void fileWriteContent() throws IOException {
         File directory = new File("C:\\Users\\Anton\\Desktop\\test");
         Scanner scanner = new Scanner(System.in);
         String fileName = scanner.next();
@@ -40,8 +39,38 @@ public class Main {
         Files.writeString(path, content, StandardCharsets.UTF_8);
     }
 
-    public static void main(String[] args) throws IOException {
+    static byte[] readBytes(String filename) throws IOException {
+        InputStream inputStream = new FileInputStream(filename);
 
+        byte[] buffer = new byte[inputStream.available()]; // {0}
+        System.out.printf("Available %d bytes%n", inputStream.available());
+        // d - digit
+        // s - string
+        // f - float
+        // n - new line
+        // List<Byte[]> list = new ArrayList<>();
+        int count = 0;
+        while (inputStream.available() > 0)
+        {
+            count = inputStream.read(buffer);
+        }
+
+        System.out.printf("Read %d bytes%n", count);
+
+        inputStream.close();
+        return buffer;
+    }
+
+    static void writeBytes(byte[] bytes, String filename) throws IOException {
+        OutputStream outputStream = new FileOutputStream(filename);
+        outputStream.write(bytes);
+        System.out.printf("Written %d bytes%n", bytes.length);
+        outputStream.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        byte[] bytes = readBytes("C:\\Users\\Anton\\Desktop\\test\\file.txt");
+        writeBytes(bytes, "C:\\Users\\Anton\\Desktop\\test\\outputFile.txt");
     }
 
 }
