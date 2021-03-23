3f36aea8cacc16f2ae362859be7fea0f7cc9b50f
Anton Zhuk <54962677+anton-zhuk99@users.noreply.github.com>
2021-03-23 20:32:16 +0200

    Add synchronized examples examples.

---
+++ src/com/company/examples/module2/ex021_concurrency/ArraySumator.java
@@ -0,0 +1,16 @@
+package com.company.examples.module2.ex021_concurrency;
+
+public class ArraySumator {
+
+    private int sum;
+
+    public int sum(int[] arr) {
+        sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            System.out.println("Thread:" + Thread.currentThread().getName() + ", sum = " + sum + ", arr[" + i + "] = " + arr[i]);
+            Main.sleep(10);
+        }
+        return sum;
+    }
+}
---
+++ src/com/company/examples/module2/ex021_concurrency/SumArrayThread.java
@@ -0,0 +1,23 @@
+package com.company.examples.module2.ex021_concurrency;
+
+public class SumArrayThread extends Thread {
+
+    private int[] arr;
+    private static ArraySumator as;
+    private int answer;
+
+    public SumArrayThread(String name, int[] arr) {
+        super(name);
+        this.arr = arr;
+        as = new ArraySumator();
+        super.start();
+    }
+
+    @Override
+    public void run() {
+        synchronized (as) {
+            answer = as.sum(this.arr);
+        }
+        System.out.println("In thread " + Thread.currentThread().getName() + " answer = " + answer);
+    }
+}
