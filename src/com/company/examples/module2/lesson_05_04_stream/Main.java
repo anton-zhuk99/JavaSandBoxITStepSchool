947494be6904f004168711426c4fd13f4288436b
Anton Zhuk <54962677+anton-zhuk99@users.noreply.github.com>
2021-04-05 21:22:42 +0300

    Add Stream examples.

--- src/com/company/examples/module2/lesson_05_04_stream/Main.java
+++ src/com/company/examples/module2/lesson_05_04_stream/Main.java
@@ -23,12 +23,11 @@ public class Main {
 
     public static void main(String[] args) {
         List<String> list = List.of("first", "second", "third", "1234567");
-        String reduction = list.stream()                 // Stream<String>: "first", "second", "third"
-                //.map(s -> s.length()) // Stream<Integer>: 5, 6, 5
-                //.filter(i -> i > 5)
-                //.sorted((s1, s2) -> s1.length() - s2.length()) // Comparator
+
+        String reduction = list.stream()
                 .reduce("Identity", (s1, s2) -> s1 + ", " + s2);
         System.out.println(reduction);
+
         List<String> sortedList = list.stream()
                 .sorted((s1, s2) -> s1.length() - s2.length())
                 .collect(Collectors.toList());
